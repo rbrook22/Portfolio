a98085ad7582f949c4b687bd951022f54d789daf
Richard Brooks <rickbrooks22@gmail.com>
2018-06-23 17:07:00 -0400

    added normalize link to rad html page, added changing background color on navbar when scrolling

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ $(document).ready(function() {
         $("body").toggleClass("open");
     });
 });
+
+$(document).scroll(function(){
+    var height = $(".navOptions").height();
+
+    if($(this).scrollTop() > height){
+        $(".navOptions").css("background-color", "rgba(183, 213, 245, 0.8)");
+    } else{
+        $(".navOptions").css("background-color", "rgb(242, 240, 234)");
+    };
+});
