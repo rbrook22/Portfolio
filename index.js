ee66102e7cc58cc9e8072c39b540bf0be91df701
Richard Brooks <rickbrooks22@gmail.com>
2018-07-16 16:23:04 -0400

    fixed navbar discoloring issue

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-var dontDoThing = true;
 $(document).ready(function() {
     $(".menuIcon, .menuWord").on("click", function(){
         $("body").toggleClass("open");
-        dontDoThing=false;
     });
 });
 
 // NavBar Scroll Change Color
 $(document).scroll(function(){
     var height = $(".navOptions").height();
-    if(dontDoThing){
-        if($(this).scrollTop() > height){
-            $(".navOptions").css("background-color", "rgba(183, 213, 245, 0.8)");
-        } else{
-            $(".navOptions").css("background-color", "rgb(242, 240, 234)");
-        }
+    if ($(this).scrollTop() > height) {
+        $(".navOptions").css("background-color", "rgba(183, 213, 245, 0.8)");
+    } else {
+        $(".navOptions").css("background-color", "rgb(242, 240, 234)");
     }
 });
 
