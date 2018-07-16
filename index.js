10bfd541a80c42b30ce8f46875036b9b3014e924
Richard Brooks <rickbrooks22@gmail.com>
2018-07-16 15:48:19 -0400

    changed js file

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+var dontDoThing = true;
 $(document).ready(function() {
-    $(".menuIcon").on("click", function(){
+    $(".menuIcon, .menuWord").on("click", function(){
         $("body").toggleClass("open");
+        dontDoThing=false;
     });
 });
+
 // NavBar Scroll Change Color
 $(document).scroll(function(){
     var height = $(".navOptions").height();
-
-    if($(this).scrollTop() > height){
-        $(".navOptions").css("background-color", "rgba(183, 213, 245, 0.8)");
-    } else{
-        $(".navOptions").css("background-color", "rgb(242, 240, 234)");
-    };
+    if(dontDoThing){
+        if($(this).scrollTop() > height){
+            $(".navOptions").css("background-color", "rgba(183, 213, 245, 0.8)");
+        } else{
+            $(".navOptions").css("background-color", "rgb(242, 240, 234)");
+        }
+    }
 });
+
 // Homepage Logo Ease In
 $(function() {
     $('.imgMain').removeClass('fade-out');
